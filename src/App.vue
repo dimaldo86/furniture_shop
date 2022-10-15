347efd7cddd1a2cfc19140d33c3c98ce98e8924d
dimaldo86 <dimaldo86ne@mail.ru>
2022-10-16 04:46:35 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  </div>
+
+</template>
+
+<style>
+
+</style>
